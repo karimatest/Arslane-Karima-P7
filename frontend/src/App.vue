48c8a05f3f6cfad976638aa1e4a9962b1ffc250b
karima arslane <kamime.93@hotmail.fr>
2022-11-08 14:44:14 +0100

    creation toutes lespublications

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,11 +9,12 @@
 import PageFooter from './components/PageFooter.vue'
 
 
+
 export default {
   name: 'App',
-    components: {
-        PageFooter,
-    },
+    components: {  
+    PageFooter
+},
   
 }
 </script>
@@ -25,7 +26,7 @@ font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50
+  
 }
 </style>
